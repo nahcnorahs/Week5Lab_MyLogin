6920526853f2ad982ef623039a491f9199d309fe
Sharon <>
2022-10-05 11:57:37 -0600

    Had to create separate class to include User and AccountService

--- src/java/models/AccountService.java
+++ src/java/models/AccountService.java
@@ -6,40 +6,10 @@ package models;
  * @author Sharon
  */
 public class AccountService {
-        private String username;
-        private String password = "password";
-       
-        
-    public class User {
-    }
-    
-    public AccountService(){
-        
-    }
-    
-    public String getUsername(){
-        return username;
-    }
-   
-    
-    public void setUsername(String username){
-        this.username = username;
-    }
      
-     
-    public String getPassword(){
-        return password;
-    }
-    
-    public void setPassword(String password){
-        this.password = password;
-    }
-    
-    public User login(String username, String pasword){
+    public User login(String username, String password){
        if (password.equals("password") && username.equals("abe") || username.equals("barb")){
-           AccountService user = new User(username,password);
-           user.setPassword(null);
-           return user;
+           return new User(username,null);
        } else {
            return null;
        }
